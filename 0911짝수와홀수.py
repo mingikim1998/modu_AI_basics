99c5d920ccdb091cbeecaa2ad861f0ef86fcd6d0
mingikim1998 <babel5165@gmail.com>
2020-09-11 21:12:05 +0900

    EZ

---
+++ 0911짝수와홀수.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def solution(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+# In[2]:
+
+
+solution(3)
+
+
+# In[3]:
+
+
+solution(4)
+
+
+# In[ ]:
+
+
+
+
