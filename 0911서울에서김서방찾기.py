f70b2b3f2efcfaec420acb8d783a7269bf2640e6
mingikim1998 <babel5165@gmail.com>
2020-09-11 21:01:19 +0900

    5mins?

---
+++ 0911서울에서김서방찾기.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def s
+for i in enumerate(seoul) :
+    if i[-1] == "Kim":
+        print("김서방은 %d에 있다" % i[0])  
+
+
+# In[2]:
+
+
+def solution(seoul):
+    for i in enumerate(seoul):
+        if i[-1] == "Kim":
+            return("김서방은 %d에 있다" % i[0])
+
+
+# In[3]:
+
+
+seoul = ['Jane', 'Kim']
+
+
+# In[4]:
+
+
+solution(seoul)
+
+
+# In[ ]:
+
+
+
+
